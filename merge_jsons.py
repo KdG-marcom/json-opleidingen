2aad2f39780ac0ce36f6e0ba4130f458b8ce40f0
KdG-marcom <webmaster@kdg.be>
2025-06-03 07:30:59 +0200

    Update merge_jsons.py

--- merge_jsons.py
+++ merge_jsons.py
@@ -4,7 +4,7 @@ import json
 input_folder = 'per-opleidingstype'
 output_file = 'DEF-test.json'
 
-merged_data = {}
+merged_data = []
 
 for filename in os.listdir(input_folder):
     if filename.endswith('.json'):
@@ -12,8 +12,7 @@ for filename in os.listdir(input_folder):
         with open(filepath, 'r', encoding='utf-8') as f:
             try:
                 content = json.load(f)
-                key = filename.replace('.json', '')
-                merged_data[key] = content
+                merged_data.append(content)
             except json.JSONDecodeError as e:
                 print(f"Fout in {filename}: {e}")
 
