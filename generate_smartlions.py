de0030feca48d5a42d02ab0888300ef0c341afd0
KdG-marcom <webmaster@kdg.be>
2025-06-03 13:02:00 +0200

    Update generate_smartlions.py

--- generate_smartlions.py
+++ generate_smartlions.py
@@ -6,7 +6,6 @@ output_file = 'DEF-Smartlions.json'
 with open(input_file, 'r', encoding='utf-8') as f:
     data = json.load(f)
 
-# Pas webaddress aan
 for item in data:
     if "webaddress" in item and isinstance(item["webaddress"], str):
         item["webaddress"] = item["webaddress"].replace("utm_source=jobat", "utm_source=smartlions")
