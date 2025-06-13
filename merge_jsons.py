525e811976d764702b251d7a2311e9076c6f95ec
KdG-marcom <webmaster@kdg.be>
2025-06-13 06:12:50 +0200

    Update merge_jsons.py

--- merge_jsons.py
+++ merge_jsons.py
@@ -9,19 +9,19 @@ merged_data = []
 # Mapping van foutieve naar correcte values
 duration_type_corrections = {
     "year": ("years", None),
-    "semester": ("months", 5),
+    "semester": ("years", 0.5),
     "dagen": ("days", None),
     "lesweken": ("weeks", None),
     "avonden": ("days", None),
     "weken": ("weeks", None),
     "academiejaar": ("years", None),
     "weken (per module)": ("weeks", None),
-    "semester (sommige een jaar)": ("months", 5),
+    "semester (sommige een jaar)": ("years", 0.5),
     "uur (5 donderdagen)": ("hours", None),
     "maanden (voltijds)": ("months", None),
     "jaar": ("years", None),
-    "semester (8 lessen)": ("months", 5),
-    "semester (per module)": ("months", 5)
+    "semester (8 lessen)": ("years", 0.5),
+    "semester (per module)": ("years", 0.5)
 }
 
 for filename in os.listdir(input_folder):
